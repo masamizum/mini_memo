b07fb3f0ae6d48bc9e7975d02b03fb373fa0bba6
masamizum <sky.masahiro@gmail.com>
2021-10-27 21:35:57 +0900

    onlyオプションの削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'memos#index'
-  resources :memos, only: [:index, :new, :create, :destroy, :edit, :update, :show]
+  resources :memos 
+    #onlyオプションの削除
+    #, only: [:index, :new, :create, :destroy, :edit, :update, :show]
 
 end
