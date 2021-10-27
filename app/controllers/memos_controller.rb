0e281f1194c71fed738e4bccb7ce160c301b477c
masamizum <sky.masahiro@gmail.com>
2021-10-27 23:05:07 +0900

    Revert "211027"
    This reverts commit 275083556f3e1eb82f6c3bc7cee6c333947f400e.

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -1,7 +1,5 @@
 class MemosController < ApplicationController
   before_action :set_memo, only: [:edit, :show]
-  before_action :move_to_index, except: [:index, :show]
-
 
   def index
     @memos = Memo.all
@@ -40,12 +38,6 @@ class MemosController < ApplicationController
 
   def set_memo
     @memo = Memo.find(params[:id])
-  end
-
-  def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
-    end
   end
 
 end
