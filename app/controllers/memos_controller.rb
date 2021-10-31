f9984a4c47d4994d7f480b391eed82283a9e1618
masamizum <sky.masahiro@gmail.com>
2021-10-31 23:36:15 +0900

    Update memos_controller.rb

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -30,6 +30,8 @@ class MemosController < ApplicationController
 
   def show
     #@memo = Memo.find(params[:id])
+    @comment = Comment.new
+    @comments = @memo.comments.includes(:user)
   end
 
   private
