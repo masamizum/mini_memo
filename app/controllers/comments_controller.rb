a361767b943d9d76466c4d6daaec5fd56ec6860c
masamizum <sky.masahiro@gmail.com>
2021-10-31 23:30:22 +0900

    Update comments_controller.rb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,10 @@
 class CommentsController < ApplicationController
+  def create
+    Comment.create(comment_params)
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, memo_id: params[:memo_id])
+  end
 end
